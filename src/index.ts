ecfab6f9907d33568ee68a23eccf072da8e7ff51
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-06-30 13:02:16 +0200

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -6,106 +6,79 @@ export interface ErrorDetails {
     code?: number;
 }
 
-export interface ErrorBody {
+export interface BaseErrorBody {
+    type: string
+    raw?: string
+    error?: ErrorDetails;
+}
+
+export interface ErrorBody extends BaseErrorBody {
     colno: number;
     filename: string;
     lineno: number;
     message: string;
     type: string;
-    rawError?: string
-    error?: ErrorDetails;
 }
 
-export interface RejectionReasonBody {
-    type: string
-    rawReason?: string
-    error?: ErrorDetails;
+function getRaw(error: Error): string | undefined {
+    let rawError: string | undefined = undefined;
+    try {
+        rawError = JSON.stringify(error);
+        if (rawError === "{}") {
+            rawError = undefined;
+        }
+    } catch {}
+
+    return rawError;
 }
 
-function getErrorDetails(error: Error): ErrorDetails {
-    return {cause: error.cause, stack: error.stack, name: error.name};
+function getBaseBody(error: Error & {code?: number}): BaseErrorBody {
+    return {
+        error: {
+            cause: error.cause,
+            stack: error.stack,
+            name: error.name,
+            message: error.message,
+            code: error?.code
+        },
+        type: error?.constructor?.name ?? typeof error,
+        raw: getRaw(error),
+    };
 }
 
-function getDomExceptionDetails(error: DOMException): ErrorDetails {
-    return {message: error.message, name: error.name, code: error.code};
+function postError(path: string, body: BaseErrorBody | ErrorBody) {
+    return fetch(
+        path,
+        {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {"Content-Type": "application/json"},
+            keepalive: true,
+        }
+    );
 }
 
 export function addErrorEventListeners() {
     window.addEventListener("error", event => {
-        const error = event.error;
-
-        let rawError: string | undefined = undefined;
-        try {
-            rawError = JSON.stringify(error);
-        } catch {}
-
-        const {colno, filename, lineno, message} = event;
-        const body: ErrorBody = {
+        const {message, filename, lineno, colno, error} = event;
+        const body = {
             colno,
             filename,
             lineno,
             message,
-            type: error?.constructor?.name ?? typeof error,
-            rawError,
+            ...getBaseBody(error)
         };
 
-        let errorDetails: ErrorDetails | undefined = undefined;
-        if (error instanceof Error) {
-            errorDetails = getErrorDetails(error)
-        } else if (error instanceof DOMException) {
-            errorDetails = getDomExceptionDetails(error);
-        }
-
-        body.error = errorDetails;
-
-        fetch(
-            "/api/report/error",
-            {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: {"Content-Type": "application/json"},
-                keepalive: true,
-            }
-        ).catch(err => {
+        postError("/api/report/error", body).catch(err => {
             // TODO: Make this nicer.
-            console.error("An error occurred when reporting error: ", err);
+            console.error("An error occurred when reporting an error: ", err);
         })
     });
 
     window.addEventListener("unhandledrejection", event => {
-        const reason = event.reason;
-
-        let rawReason: string | undefined = undefined;
-        try {
-            rawReason = JSON.stringify(reason);
-        } catch {
-        }
-
-        const body: RejectionReasonBody = {
-            type: reason?.constructor?.name ?? typeof reason,
-            rawReason,
-        };
-
-        let errorDetails = undefined;
-        if (reason instanceof Error) {
-            errorDetails = getErrorDetails(reason)
-        } else if (reason instanceof DOMException) {
-            errorDetails = getDomExceptionDetails(reason);
-        }
-
-        body.error = errorDetails;
-
-        fetch(
-            "/api/report/unhandled-rejection",
-            {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: {"Content-Type": "application/json"},
-                keepalive: true,
-            }
-        ).catch(err => {
+        postError("/api/report/unhandled-rejection", getBaseBody(event.reason)).catch(err => {
             // TODO: Make this nicer.
-            console.error("An error occurred when reporting unhandled rejection: ", err);
-        })
+            console.error("An error occurred when reporting an unhandled rejection: ", err);
+        });
     })
 }
